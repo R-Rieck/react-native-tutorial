1a54d36480469af5de2fdb9f1fd1bd1ad1c67bac
Ron <ron.rieck@gmx.de>
2021-01-27 09:16:19 +0100

    animation für Nachricht hinzugefügt

--- components/chatItem.tsx
+++ components/chatItem.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Animated, Easing } from "react-native";
 import { ChatItemType } from "../types/chatItem";
 import styles from "../styles/chatItem";
 import { useEffect } from "react";
@@ -14,6 +14,16 @@ export const ChatItem = (props: ChatItemPropsType) => {
   const [isAuthenticatedUser, setIsAuthenticatedUser] = useState<boolean>(
     false
   );
+  const [animation] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      duration: 200,
+      useNativeDriver: true,
+      toValue: 1,
+      easing: (number) => Easing.ease(number),
+    }).start();
+  });
 
   useEffect(() => {
     if (chatItem.by === authenticatedUser) setIsAuthenticatedUser(true);
@@ -21,11 +31,13 @@ export const ChatItem = (props: ChatItemPropsType) => {
   }, [props]);
 
   return (
-    <View
+    <Animated.View
       style={{
         ...styles.chatItemContainer,
         alignSelf: isAuthenticatedUser ? "flex-end" : "flex-start",
         backgroundColor: isAuthenticatedUser ? "#5bcaff" : "#3DD2C4",
+        opacity: animation,
+        transform: [{ scale: animation }],
       }}
     >
       <View style={styles.messageHeaderContainer}>
@@ -43,6 +55,6 @@ export const ChatItem = (props: ChatItemPropsType) => {
         </View>
       </View>
       <Text style={styles.message}>{chatItem.message}</Text>
-    </View>
+    </Animated.View>
   );
 };
